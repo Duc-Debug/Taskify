3e655d2a4d04c9043e340078b5b7bc9672017042
Duc <giapduc2299@gmail.com>
2025-12-01 22:41:14 +0700

    Nothing

---
+++ Taskify/Models/ViewModel/TaskBoard/BoardCreateViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taskify.Models
+{
+    public class BoardCreateViewModel
+    {
+        [Required(ErrorMessage = "Board title is required")]
+        [MaxLength(100)]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Guid? TeamId { get; set; }
+        //Choose Temple: Blank, Kanban, Scrum
+        public string Template { get; set; } = "Kanban";
+    }
+    public class BoardEditViewModel
+    {
+
+    }
+}
